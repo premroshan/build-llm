1c38ea6b01a9180f4f5da38db78df1b94e4138eb
premroshan <premroshan@gmail.com>
2026-02-22 23:06:40 +0530

    tokenizing

--- embedding.py
+++ embedding.py
@@ -1,5 +1,59 @@
+import re
+from tokenizer import SimpleTokenizerV1
+from tokenizer import SimpleTokenizerV2
+
+from importlib.metadata import version
+import tiktoken
+print("tiktoken version:", version("tiktoken"))
+
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 print("Total number of character:", len(raw_text))
 print(raw_text[:99])
 
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(len(preprocessed))
+
+print(preprocessed[:30])
+
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+
+print(len(vocab.items()))
+
+for i, item in enumerate(vocab.items()):
+    print(item)
+    if i >= 50:
+        break
+
+for i, item in enumerate(list(vocab.items())[-5:]):
+    print(item)
+
+tokenizer = SimpleTokenizerV2(vocab)
+text = """"It's the last he painted, you know," 
+       Mrs. Gisburn said with pardonable pride."""
+ids = tokenizer.encode(text)
+print(ids)
+
+print(tokenizer.decode(ids))
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+print(tokenizer.encode(text))
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+     "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+
+strings = tokenizer.decode(integers)
+print(strings)
